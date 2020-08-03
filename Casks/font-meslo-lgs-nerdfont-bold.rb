294c2a1d30260df0de19c60602097aa520979446
Barry Dobson <barrydobson@users.noreply.github.com>
2020-08-03 13:42:26 +0100

    Update font-meslo-lgs-nerdfont-bold.rb

--- Casks/font-meslo-lgs-nerdfont-bold.rb
+++ Casks/font-meslo-lgs-nerdfont-bold.rb
@@ -1,6 +1,6 @@
 cask 'font-meslo-lgs-nerdfont-bold' do
   version ':latest'
-  sha256 ':no-check'
+  sha256 'ebdc86f4dbac57ec81e3b7b6f2e6fa182b4ac74e928f2035d398000404f42b71'
 
   url "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf"
   name 'Meslo for Powerline 10k'
