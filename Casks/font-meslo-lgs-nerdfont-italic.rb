f9ce7e8ba5aaa6fdb1629cb365747db68aba3b67
Barry Dobson <barrydobson@users.noreply.github.com>
2020-08-03 13:44:37 +0100

    Update font-meslo-lgs-nerdfont-italic.rb

--- Casks/font-meslo-lgs-nerdfont-italic.rb
+++ Casks/font-meslo-lgs-nerdfont-italic.rb
@@ -1,6 +1,6 @@
 cask 'font-meslo-lgs-nerdfont-italic' do
   version ':latest'
-  sha256 ':no-check'
+  sha256 'ffbb03ec5ae9be0bed5609c423f1846c59d94641c4f6a2ddf94e24f8c7958290'
 
   url "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf"
   name 'Meslo for Powerline 10k'
