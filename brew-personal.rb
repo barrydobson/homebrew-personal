9bee630cc12c299a9bb7f6ff6800c92dc7bdf9d0
Barry Dobson <barrydobson@users.noreply.github.com>
2022-11-04 09:12:37 +0000

    Update brew-personal.rb

--- brew-personal.rb
+++ brew-personal.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class BrewMycask < Formula
   homepage 'https://github.com/barrydobson/homebrew-personal'
-  url 'https://github.com/barrydobson/homebrew-personal.git', :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  url 'https://github.com/barrydobson/homebrew-personal.git', :using => GitHubGitDownloadStrategy
   def install
   end
 end
